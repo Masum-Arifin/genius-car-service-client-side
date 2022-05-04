aa98ec6e257fbaa8153affca149d26bd64ea7c04
Masum-Arifin <masumarifinbd@gmail.com>
2022-05-04 21:39:47 +0600

    new code

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import useServiceDetail from "../../../hooks/useServiceDetail";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from "../../../firebase.init";
 
 const Checkout = () => {
-    return (
-        <div>
-            <h2>Please Checkout your booking</h2>
-        </div>
-    );
+  const { serviceId } = useParams();
+  const [service] = useServiceDetail(serviceId);
+  const [user] = useAuthState(auth);
+  const handlePlaceOrder = e => {
+      e.preventDefault();
+      const order = {
+        email:user.email,
+        service: service.name,
+        serviceId: serviceId,
+        address: e.target.address.value,
+        phone: e.target.phone.value
+    }
+  }
+  return (
+    <div className="w-50 mx-auto">
+      <h2>Please Order: {service.name}</h2>
+      <form onSubmit={handlePlaceOrder}>
+        <input
+          className="w-100 mb-2"
+          type="text" value={user.displayName}
+          name="name"
+          placeholder="name"
+          required readOnly disabled
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="email" value={user.email}
+          name="email"
+          placeholder="email"
+          required readOnly disabled
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text" value={service.name}
+          name="service"
+          placeholder="service"
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="address"
+          placeholder="address"
+          required
+        />
+        <br />
+        <input
+          className="w-100 mb-2"
+          type="text"
+          name="Phone"
+          placeholder="Phone"
+          required
+        />
+        <br />
+        <input className="btn btn-primary"type="submit" value="Pleas Order" />
+      </form>
+    </div>
+  );
 };
 
 export default Checkout;
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,21 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import useServiceDetail from '../../hooks/useServiceDetail';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
-    const [services, setServices] = useState([]);
-
-    useEffect(() => {
-        const url=`http://localhost:5000/service/${serviceId}`
-      fetch(url)
-        .then((res) => res.json())
-        .then((data) => setServices(data));
-    }, [serviceId]);
+    const [service] = useServiceDetail(serviceId)
     return (
         <div>
-            <h2>Your are about to book: {services.name}</h2>
+            <h2>Your are about to book: {service.name}</h2>
             <div className='text-center'>
-                <Link to="/checkout">
+                <Link to={`/checkout/${serviceId}`}>
                     <button className='btn btn-primary'>Proceed Checkout</button>
                 </Link>
             </div>
